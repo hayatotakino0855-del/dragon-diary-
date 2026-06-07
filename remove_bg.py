a064b8485c255dd2fdde5dd8c8b6e9aa4924bed4
user <user@example.com>
2026-06-07 13:08:02 +0900

    v103: fix background transparency of egg and nest by running Python script, fix achievements to unlock based on total count rather than June 1st count

--- remove_bg.py
+++ remove_bg.py
@@ -1,18 +1,21 @@
-import os
-from rembg import remove
+from PIL import Image
 
-dragon_dir = r"c:\Users\User\OneDrive\デスクトップ\日記\assets\dragons"
+def remove_white_bg(input_path, output_path, tolerance=240):
+    img = Image.open(input_path).convert("RGBA")
+    datas = img.getdata()
 
-for filename in os.listdir(dragon_dir):
-    if filename.endswith(".png"):
-        input_path = os.path.join(dragon_dir, filename)
-        
-        with open(input_path, 'rb') as i:
-            input_data = i.read()
-            
-        output_data = remove(input_data)
-        
-        with open(input_path, 'wb') as o:
-            o.write(output_data)
-            
-        print(f"Processed: {filename}")
+    new_data = []
+    for item in datas:
+        # Change all white (also shades of white)
+        # to transparent
+        if item[0] >= tolerance and item[1] >= tolerance and item[2] >= tolerance:
+            new_data.append((255, 255, 255, 0))
+        else:
+            new_data.append(item)
+
+    img.putdata(new_data)
+    img.save(output_path, "PNG")
+
+remove_white_bg('assets/dragons/stage2_egg_only.png', 'assets/dragons/stage2_egg_only.png')
+remove_white_bg('assets/dragons/stage2_nest.png', 'assets/dragons/stage2_nest.png')
+print("Done")
