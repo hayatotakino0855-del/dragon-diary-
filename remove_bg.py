cb3f4e39af52fd20f79cf968b12d913a2875ab1d
user <user@example.com>
2026-06-07 13:51:06 +0900

    v107: replace stage2 egg and nest images with the newly uploaded versions, apply background removal

--- remove_bg.py
+++ remove_bg.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 
 def remove_white_bg(input_path, output_path, tolerance=240):
     img = Image.open(input_path).convert("RGBA")
@@ -6,8 +7,6 @@ def remove_white_bg(input_path, output_path, tolerance=240):
 
     new_data = []
     for item in datas:
-        # Change all white (also shades of white)
-        # to transparent
         if item[0] >= tolerance and item[1] >= tolerance and item[2] >= tolerance:
             new_data.append((255, 255, 255, 0))
         else:
@@ -16,6 +15,6 @@ def remove_white_bg(input_path, output_path, tolerance=240):
     img.putdata(new_data)
     img.save(output_path, "PNG")
 
-remove_white_bg('assets/dragons/stage2_egg_only.png', 'assets/dragons/stage2_egg_only.png')
-remove_white_bg('assets/dragons/stage2_nest.png', 'assets/dragons/stage2_nest.png')
-print("Done")
+remove_white_bg('assets/dragons/ひびの卵.jpg', 'assets/dragons/stage2_egg_only.png')
+remove_white_bg('assets/dragons/卵の藁.jpg', 'assets/dragons/stage2_nest.png')
+print("Done processing new images")
